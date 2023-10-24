fd877302b05ddaa95dc1ad340311fc5a942e3916
lPatricio_VargasR <1722110158@utectulancingo.edu.mx>
2023-10-23 20:21:52 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,289 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:sqlitedb2/convert_utility.dart';
+import 'package:sqlitedb2/dbManager.dart';
+import 'package:sqlitedb2/student.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<List<Student>>? Studentss;
+  TextEditingController telController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController apepaController = TextEditingController();
+  TextEditingController apemaController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController controlNumController = TextEditingController();
+  String? tel = '';
+  String? name = '';
+  String? email = '';
+  String? apepa = '';
+  String? apema = '';
+  String? photoname = '';
+
+  //Update control
+  int? currentUserId;
+  final formKey = GlobalKey<FormState>();
+  late var dbHelper;
+  late bool isUpdating;
+
+  //Metodos de usuario
+  refreshList() {
+    setState(() {
+      Studentss = dbHelper.getStudents();
+    });
+  }
+
+  pickImageFromGallery() {
+    ImagePicker imagePicker = ImagePicker();
+    imagePicker
+        .pickImage(source: ImageSource.gallery, maxHeight: 480, maxWidth: 640)
+        .then((value) async {
+      Uint8List? imageBytes = await value!.readAsBytes();
+      setState(() {
+        photoname = Utility.base64String(imageBytes!);
+      });
+    });
+  }
+
+  clearFields() {
+    telController.text = '';
+    nameController.text = '';
+    apepaController.text = '';
+    apemaController.text = '';
+    emailController.text = '';
+    controlNumController.text = '';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DBManager();
+    refreshList();
+    isUpdating = false;
+  }
+
+  Widget userForm() {
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          verticalDirection: VerticalDirection.down,
+          children: [
+            const SizedBox(height: 10),
+            //TextFormField(
+            //controller: controlNumController,
+            //keyboardType: TextInputType.number,
+            //decoration: const InputDecoration(
+            //labelText: 'Control Number',
+            // ),
+            //validator: (val) => val!.isEmpty ? 'Enter Control Number' : null,
+            //onSaved: (val) => controlNumController.text = val!,
+            // ),
+            TextFormField(
+              controller: nameController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                labelText: 'Name',
+              ),
+              validator: (val) => val!.isEmpty ? 'Enter Name' : null,
+              onSaved: (val) => name = val!,
+            ),
+            TextFormField(
+              controller: apepaController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                labelText: 'Apellido Paterno',
+              ),
+              validator: (val) => val!.isEmpty ? 'Ape Paterno' : null,
+              onSaved: (val) => apepa = val!,
+            ),
+            TextFormField(
+              controller: apemaController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                labelText: 'Apellido Materno',
+              ),
+              validator: (val) => val!.isEmpty ? 'Ape Materno' : null,
+              onSaved: (val) => apema = val!,
+            ),
+            TextFormField(
+              controller: telController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                labelText: 'Telefono',
+              ),
+              validator: (val) => val!.isEmpty ? 'Tel' : null,
+              onSaved: (val) => tel = val!,
+            ),
+            TextFormField(
+              controller: emailController,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+              validator: (val) => val!.isEmpty ? 'Enter Email' : null,
+              onSaved: (val) => email = val!,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  onPressed: validate,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      side: const BorderSide(color: Colors.red)),
+                  child: Text(isUpdating ? "Actualizar" : "Insertar"),
+                ),
+                MaterialButton(
+                  onPressed: (){
+                    pickImageFromGallery();
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      side: const BorderSide(color: Colors.green)),
+                  child: const Text("Seleccionar imagen"),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SingleChildScrollView userDataTable(List<Student>? Studentss){
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: DataTable(
+        columns: const [
+          DataColumn(label: Text('Photo')),
+          DataColumn(label: Text('Name')),
+          DataColumn(label: Text('Paterno')),
+          DataColumn(label: Text('Materno')),
+          DataColumn(label: Text('E-mail')),
+          DataColumn(label: Text('Telefono')),
+          DataColumn(label: Text('Delete')),
+        ],
+        rows: Studentss!.map((student)=>DataRow(cells: [
+          DataCell(Container(
+            width: 80,
+            height: 120,
+            child: Utility.ImageFromBase64String(student.photoName!),
+          )),
+          DataCell(Text(student.name!), onTap: (){
+            setState(() {
+              isUpdating = true;
+              currentUserId = student.controlNum;
+            });
+            nameController.text = student.name!;
+            apepaController.text = student.apepa!;
+            apemaController.text = student.apema!;
+            emailController.text = student.email!;
+            telController.text = student.tel!;
+          }),
+          DataCell(Text(student.apepa!)),
+          DataCell(Text(student.apema!)),
+          DataCell(Text(student.email!)),
+          DataCell(Text(student.tel!)),
+          DataCell(IconButton(
+            onPressed: () {
+              dbHelper.delete(student.controlNum);
+              refreshList();
+            },
+            icon: const Icon(Icons.delete),
+          ))
+        ])).toList(),
+      ),
+    );
+  }
+
+  Widget list (){
+    return Expanded(
+        child: SingleChildScrollView(
+          child: FutureBuilder(
+              future: Studentss,
+              builder: (context, AsyncSnapshot<dynamic> snapshot){
+                if(snapshot.hasData){
+                  print(snapshot.data);
+                  return userDataTable(snapshot.data);
+                }
+                if(!snapshot.hasData){
+                  print("Data Not Found");
+                }
+                return const CircularProgressIndicator();
+              }),
+        ));
+  }
+
+  validate(){
+    if(formKey.currentState!.validate()){
+      formKey.currentState!.save();
+      if(isUpdating){
+        Student student = Student(
+            controlNum: currentUserId,
+            name: name,
+            apepa: apepa,
+            apema: apema,
+            email: email,
+            tel: tel,
+            photoName: photoname);
+        dbHelper.update(student);
+        isUpdating = false;
+      } else{
+        Student student = Student(
+            controlNum: null,
+            name: name,
+            apepa: apepa,
+            apema: apema,
+            email: email,
+            tel: tel,
+            photoName: photoname);
+        dbHelper.save(student);
+      }
+      clearFields();
+      refreshList();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SQLite DB'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        verticalDirection: VerticalDirection.down,
+        children: [userForm(),list()],
+      ),
+    );
+  }
+}
